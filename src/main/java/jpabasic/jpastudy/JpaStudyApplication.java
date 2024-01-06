5ad65e224047612175cf32a0bb4adb273c5ee0af
MoonByungHoon <mbh1069@naver.com>
2024-01-07 08:10:44 +0900

    2024-01-07

--- src/main/java/jpabasic/jpastudy/JpaStudyApplication.java
+++ src/main/java/jpabasic/jpastudy/JpaStudyApplication.java
@@ -4,8 +4,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabasic.jpastudy.domain.Child;
 import jpabasic.jpastudy.domain.Order;
 import jpabasic.jpastudy.domain.OrderItem;
+import jpabasic.jpastudy.domain.Parent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -21,8 +23,16 @@ public class JpaStudyApplication {
     tx.begin();
 
     try {
+      Child child1 = new Child();
+      Child child2 = new Child();
 
+      Parent parent = new Parent();
+      parent.addChild(child1);
+      parent.addChild(child2);
 
+      em.persist(parent);
+
+      tx.commit();
     } catch (Exception e) {
       tx.rollback();
     } finally {
--- src/main/java/jpabasic/jpastudy/impl/Member.java
+++ src/main/java/jpabasic/jpastudy/impl/Member.java
@@ -11,7 +11,7 @@ public class Member {
 
   private String username;
 
-  @ManyToOne(fetch = FetchType.LAZY)
+  @ManyToOne(fetch = FetchType.EAGER)
   @JoinColumn
   private Team team;
 }
